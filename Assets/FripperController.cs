acf42308126c7887971075ef3ebb7a5b9cf44a8f
HirokiYamamoto <futsalhiro@gmail.com>
2017-01-14 14:28:52 -0800

    Lesson5課題
    チェックお願いします

---
+++ Assets/FripperController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+	//HingeJointコンポーネントを入れる
+	private HingeJoint myHingeJoynt;
+	//初期の傾き
+	private float defaultAngle = 20f;
+	//弾いた時の傾き
+	private float flickAngle = -20f;
+	private float center = Screen.width / 2;
+
+
+	// Use this for initialization
+	void Start () {
+		//HingeJointコンポーンネントを取得
+		this.myHingeJoynt = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < Input.touchCount; i++) {
+
+			Touch touch = Input.touches [i];
+			float touchPosition = touch.position.x;
+
+			//左キーを押した時左フリッパーを動かす
+			if (touchPosition < center && tag == "LeftFripperTag") {
+				if (touch.phase == TouchPhase.Began) {
+					SetAngle (this.flickAngle);
+				}
+				if (touch.phase == TouchPhase.Ended) {
+					SetAngle (this.defaultAngle);
+				}
+			}
+
+			//右キーを押した時右フリッパーを動かす
+			if (touchPosition > center && tag == "RightFripperTag") {
+				if (touch.phase == TouchPhase.Began) {
+					SetAngle (this.flickAngle);
+				}
+				if (touch.phase == TouchPhase.Ended) {
+					SetAngle (this.defaultAngle);
+				}
+			}
+		}
+	}
+
+	//フリッパーの傾きを設定
+	public void SetAngle(float angle) {
+		JointSpring jointSpr = this.myHingeJoynt.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoynt.spring = jointSpr;
+	}
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+	private GameObject scoreText;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.scoreText = GameObject.Find ("ScoreText");
+	}
+		
+void OnCollisionEnter(Collision other) {
+
+	    if (other.gameObject.tag == "SmallStarTag") {
+			this.score += 5;
+		} else if (other.gameObject.tag == "LargeStarTag"){
+			this.score += 10;
+		} else if (other.gameObject.tag == "SmallCloudTag"){
+			this.score += 25;
+		} else if (other.gameObject.tag == "LargeCloudTag"){
+			this.score += 20;
+	    }
+		this.scoreText.GetComponent<Text> ().text = "Score " + this.score + "pt";
+
+}
+
+}
